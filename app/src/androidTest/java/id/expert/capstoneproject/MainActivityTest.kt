e19686707858d97e931404fcdf346252c6aa1a49
AnanditaRian19 <rizkianandita6@gmail.com>
2021-04-29 20:32:05 +0700

    Add: image asset

---
+++ app/src/androidTest/java/id/expert/capstoneproject/MainActivityTest.kt
@@ -0,0 +1,20 @@
+package id.expert.capstoneproject
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeUp
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun moviesFragment() {
+        onView(withId(R.id.scroll_view)).perform(swipeUp())
+    }
+
+}
