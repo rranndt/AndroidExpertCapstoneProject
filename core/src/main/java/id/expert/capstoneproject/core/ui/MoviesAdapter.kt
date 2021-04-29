43e81a97b9aa8a5ae5982a2f39e0f830082d53e6
AnanditaRian19 <rizkianandita6@gmail.com>
2021-04-29 20:40:06 +0700

    Change: modified some class and variable name

--- core/src/main/java/id/expert/capstoneproject/core/ui/MoviesAdapter.kt
+++ core/src/main/java/id/expert/capstoneproject/core/ui/MoviesAdapter.kt
@@ -1,14 +1,10 @@
 package id.expert.capstoneproject.core.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import id.expert.capstoneproject.core.BuildConfig.IMAGE_URL
-import id.expert.capstoneproject.core.R
 import id.expert.capstoneproject.core.databinding.RecyclerItemGridBinding
 import id.expert.capstoneproject.core.domain.model.Movies
-import id.expert.capstoneproject.core.utils.Helper
 
 class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.ListViewHolder>() {
 
@@ -26,9 +22,9 @@ class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.ListViewHolder>() {
         parent: ViewGroup,
         viewType: Int
     ): MoviesAdapter.ListViewHolder {
-        return ListViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_grid, parent, false)
-        )
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = RecyclerItemGridBinding.inflate(layoutInflater, parent, false)
+        return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MoviesAdapter.ListViewHolder, position: Int) {
@@ -40,18 +36,12 @@ class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.ListViewHolder>() {
         return listData.size
     }
 
-    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding = RecyclerItemGridBinding.bind(itemView)
+    inner class ListViewHolder(private val binding: RecyclerItemGridBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(data: Movies) {
             with(binding) {
-                Helper.setGlide(
-                    itemView.context,
-                    "$IMAGE_URL${data.posterPath}",
-                    ivPoster
-                )
-                tvTitle.text = data.title
-                ratingBar.rating = data.voteAverage / 2
-                tvRating.text = data.voteAverage.toString()
+                result = data
+                executePendingBindings()
             }
         }
 
--- core/src/main/java/id/expert/capstoneproject/core/ui/MoviesSimilarAdapter.kt
+++ core/src/main/java/id/expert/capstoneproject/core/ui/MoviesSimilarAdapter.kt
@@ -1,14 +1,10 @@
 package id.expert.capstoneproject.core.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import id.expert.capstoneproject.core.BuildConfig.IMAGE_URL
-import id.expert.capstoneproject.core.R
 import id.expert.capstoneproject.core.databinding.RecyclerItemGridSimilarMoviesBinding
 import id.expert.capstoneproject.core.domain.model.Movies
-import id.expert.capstoneproject.core.utils.Helper
 
 class MoviesSimilarAdapter : RecyclerView.Adapter<MoviesSimilarAdapter.ListViewHolder>() {
 
@@ -26,9 +22,9 @@ class MoviesSimilarAdapter : RecyclerView.Adapter<MoviesSimilarAdapter.ListViewH
         parent: ViewGroup,
         viewType: Int
     ): MoviesSimilarAdapter.ListViewHolder {
-        return ListViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_grid_similar_movies, parent, false)
-        )
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = RecyclerItemGridSimilarMoviesBinding.inflate(layoutInflater, parent, false)
+        return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MoviesSimilarAdapter.ListViewHolder, position: Int) {
@@ -40,18 +36,12 @@ class MoviesSimilarAdapter : RecyclerView.Adapter<MoviesSimilarAdapter.ListViewH
         return listData.size
     }
 
-    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding = RecyclerItemGridSimilarMoviesBinding.bind(itemView)
+    inner class ListViewHolder(private val binding: RecyclerItemGridSimilarMoviesBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(data: Movies) {
             with(binding) {
-                Helper.setGlide(
-                    itemView.context,
-                    "$IMAGE_URL${data.posterPath}",
-                    ivPoster
-                )
-                tvTitle.text = data.title
-                ratingBar.rating = data.voteAverage / 2
-                tvRating.text = data.voteAverage.toString()
+                result = data
+                executePendingBindings()
             }
         }
 
