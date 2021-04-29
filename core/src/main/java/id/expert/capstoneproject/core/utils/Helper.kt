48d002b937c66ee2fd647f0dabbf1077b571b32e
AnanditaRian19 <rizkianandita6@gmail.com>
2021-04-29 20:39:17 +0700

    Delete: unused class

--- core/src/main/java/id/expert/capstoneproject/core/utils/Helper.kt
+++ core/src/main/java/id/expert/capstoneproject/core/utils/Helper.kt
@@ -1,12 +1,6 @@
 package id.expert.capstoneproject.core.utils
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import id.expert.capstoneproject.core.R
 import java.text.SimpleDateFormat
 
 object Helper {
@@ -21,25 +15,6 @@ object Helper {
         val newFormatDate = SimpleDateFormat(requiredFormat)
         val updateDate = oldFormatDate.parse(dateString)
         return newFormatDate.format(updateDate!!)
-    }
-
-    fun setGlide(context: Context, imageUrl: String, imageView: ImageView) {
-        Glide.with(context)
-            .load(imageUrl)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .placeholder(R.drawable.ic_loading_placeholder)
-            .error(R.drawable.ic_placeholder_error)
-            .into(imageView)
-    }
-
-    fun setGlideWithRadius(context: Context, imageUrl: String, imageView: ImageView) {
-        Glide.with(context)
-            .load(imageUrl)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .transform(RoundedCorners(16))
-            .placeholder(R.drawable.ic_loading_placeholder)
-            .error(R.drawable.ic_placeholder_error)
-            .into(imageView)
     }
 
 }
